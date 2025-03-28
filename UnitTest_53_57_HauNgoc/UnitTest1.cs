144e4b2b58eab5e0170e74b8aa6ff8d96a45654e
lethiyenngoc <x.uyen050519@gmail.com>
2025-03-28 23:43:49 +0700

    Update UnitTest1

--- UnitTest_53_57_HauNgoc/UnitTest1.cs
+++ UnitTest_53_57_HauNgoc/UnitTest1.cs
@@ -51,5 +51,41 @@ namespace UnitTest_53_57_HauNgoc
             String expected_53_57_HauNgoc = "x1 = 3, x2 = 2.45";
             Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
+
+        [TestMethod]
+        public void TC6_NghiemKep_53_57_HauNgoc_Fail()
+        {
+            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, -4, 4);
+            string actualResult = ptbac2.Solve_53_57_HauNgoc();
+            string expectedResult = "Nghiệm kép x = 3.00";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TC7_PTVoNghiem_53_57_HauNgoc_Fail()
+        {
+            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, 2, 1);
+            string actualResult = ptbac2.Solve_53_57_HauNgoc();
+            string expectedResult = "Phương trình vô nghiệm";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TC8_PTVoSoNghiem_53_57_HauNgoc_Fail()
+        {
+            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(0, 0, 0);
+            string actualResult = ptbac2.Solve_53_57_HauNgoc();
+            string expectedResult = "Phương trình có một nghiệm duy nhất";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TC9_2NghiemPB_53_57_HauNgoc_Fail()
+        {
+            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, -5, 6);
+            string actualResult = ptbac2.Solve_53_57_HauNgoc();
+            string expectedResult = "x1 = 1.00, x2 = 2.00";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
