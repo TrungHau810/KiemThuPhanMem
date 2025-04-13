a10c6cc3971c46283506f581c27c77b75abae769
trunghau810 <tthau2004@gmail.com>
2025-04-13 15:49:24 +0700

    Update Unit test

--- NUnit_53_57_HauNgoc/GiaiPhuongTrinhBac2_53_57_HauNgoc/PTBac2_53_57_HauNgoc.cs
+++ NUnit_53_57_HauNgoc/GiaiPhuongTrinhBac2_53_57_HauNgoc/PTBac2_53_57_HauNgoc.cs
@@ -1,53 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace GiaiPhuongTrinhBac2_53_57_HauNgoc
-//{
-//    public class PTBac2_53_57_HauNgoc
-//    {
-//        double a_53_57_HauNgoc, b_53_57_HauNgoc, c_53_57_HauNgoc;
-
-//        public PTBac2_53_57_HauNgoc(double a_53_57_HauNgoc, double b_53_57_HauNgoc, double c_53_57_HauNgoc)
-//        {
-//            this.a_53_57_HauNgoc = a_53_57_HauNgoc;
-//            this.b_53_57_HauNgoc = b_53_57_HauNgoc;
-//            this.c_53_57_HauNgoc = c_53_57_HauNgoc;
-//        }
-
-//        public string Solve_53_57_HauNgoc()
-//        {
-//            if (this.a_53_57_HauNgoc == 0)
-//            {
-//                if (this.b_53_57_HauNgoc == 0)
-//                    return (this.c_53_57_HauNgoc == 0) ? "Phương trình có vô số nghiệm" : "Phương trình vô nghiệm";
-
-//                double x_53_57_HauNgoc = -this.c_53_57_HauNgoc / this.b_53_57_HauNgoc;
-//                return $"x = {x_53_57_HauNgoc.ToString("0.##")}";
-//            }
-
-//            double delta_53_57_HauNgoc = Math.Pow(this.b_53_57_HauNgoc, 2) - 4 * this.a_53_57_HauNgoc * this.c_53_57_HauNgoc;
-
-//            if (delta_53_57_HauNgoc < 0)
-//                return "Phương trình vô nghiệm";
-
-//            if (delta_53_57_HauNgoc == 0)
-//            {
-//                double x_53_57_HauNgoc = -this.b_53_57_HauNgoc / (2 * this.a_53_57_HauNgoc);
-//                return $"Nghiệm kép x = {x_53_57_HauNgoc.ToString("0.##")}";
-//            }
-
-//            double x1_53_57_HauNgoc = (-this.b_53_57_HauNgoc + Math.Sqrt(delta_53_57_HauNgoc)) / (2 * this.a_53_57_HauNgoc);
-//            double x2_53_57_HauNgoc = (-this.b_53_57_HauNgoc - Math.Sqrt(delta_53_57_HauNgoc)) / (2 * this.a_53_57_HauNgoc);
-//            return $"x1 = {x1_53_57_HauNgoc.ToString("0.##")}, x2 = {x2_53_57_HauNgoc.ToString("0.##")}";
-//        }
-//    }
-//}
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -64,37 +15,51 @@ namespace GiaiPhuongTrinhBac2_53_57_HauNgoc
             this.b_53_57_HauNgoc = b_53_57_HauNgoc;
             this.c_53_57_HauNgoc = c_53_57_HauNgoc;
         }
+
+        // Hàm dùng để fommat lại kết quả nghiệm xuất ra màn hình
+        private string FormatNumber_53_57_HauNgoc(double number_53_57_HauNgoc)
+        {
+            // G (General Format): Nếu số là số nguyên, nó sẽ k hiển thị dấu phẩy (2 thay vì 2.00)
+            // Nếu x là số thập phân sẽ tự động làm tròn 2 chữ số thập phân (như 2.53)
+            // Nếu là 1 chữ số thập phân như 0.5000 -> 0.5
+            return (number_53_57_HauNgoc % 1 == 0) ? number_53_57_HauNgoc.ToString("G") : number_53_57_HauNgoc.ToString("0.##");
+        }
+
         public string Solve_53_57_HauNgoc()
         {
             if (this.a_53_57_HauNgoc == 0)
             {
-                // bx + c = 0
+                // a = 0 -> Trở thành phương trình bậc 1: bx + c = 0
+                // Nếu b = 0
                 if (this.b_53_57_HauNgoc == 0)
-                    return (this.c_53_57_HauNgoc == 0) ? "Phương trình có vô số nghiệm" : "Phương trình vô nghiệm";
-
-                // G (General Format): Nếu số là số nguyên, nó sẽ k hiển thị dấu phẩy (2 thay vì 2.00)
-                // Nếu x là số thập phân sẽ tự động làm tròn 2 chữ số thập phân (như 2.53)
+                    // Nếu c = 0 -> Phương trình có vô số nghiệm, ngược lại thì phương trình vô nghiệm
+                    return (this.c_53_57_HauNgoc == 0) ? "Phuong trinh co vo so nghiem" : "Phuong trinh vo nghiem";
+                // Nếu b khác 0
+                // Tính nghiệm x của phương trình bậc 1 bằng công thức -c/b
                 double x_53_57_HauNgoc = (-this.c_53_57_HauNgoc / this.b_53_57_HauNgoc);
-                return $"x = {(x_53_57_HauNgoc % 1 == 0 ? x_53_57_HauNgoc.ToString("G") : x_53_57_HauNgoc.ToString("0.##"))}";
+                // Trả về kết quả nghiệm x = ...
+                return $"x = {FormatNumber_53_57_HauNgoc(x_53_57_HauNgoc)}";
             }
             // Tính delta = b^2 - 4ac
             double delta_53_57_HauNgoc = (this.b_53_57_HauNgoc * this.b_53_57_HauNgoc) - (4 * this.a_53_57_HauNgoc * this.c_53_57_HauNgoc);
+            // TH1: delta < 0 -> Phương trình vô nghiệm
             if (delta_53_57_HauNgoc < 0)
-                return "Phương trình vô nghiệm";
-
+                return "Phuong trinh vo nghiem";
+            // TH2: delta = 0 -> Phương trình có nghiệm kép x = -b/2a
             else if (delta_53_57_HauNgoc == 0)
             {
                 double x_53_57_HauNgoc = -this.b_53_57_HauNgoc / (2 * this.a_53_57_HauNgoc);
-                return $"Nghiệm kép x = {(x_53_57_HauNgoc % 1 == 0 ? x_53_57_HauNgoc.ToString("G") : x_53_57_HauNgoc.ToString("0.##"))}";
+                return $"Nghiem kep x = {FormatNumber_53_57_HauNgoc(x_53_57_HauNgoc)}";
             }
-            // delta > 0
+            // TH3: delta > 0 -> Phương trình có 2 nghiệm phân biệt
             else
             {
+                // Tính 2 nghiệm phân biệt x1 và x2
                 double x1_53_57_HauNgoc = (-this.b_53_57_HauNgoc + Math.Sqrt(delta_53_57_HauNgoc)) / (2 * this.a_53_57_HauNgoc);
                 double x2_53_57_HauNgoc = (-this.b_53_57_HauNgoc - Math.Sqrt(delta_53_57_HauNgoc)) / (2 * this.a_53_57_HauNgoc);
-                // Chỉnh format x1 và x2 
-                return $"x1 = {(x1_53_57_HauNgoc % 1 == 0 ? x1_53_57_HauNgoc.ToString("G") : x1_53_57_HauNgoc.ToString("0.##"))}, " +
-                     $"x2 = {(x2_53_57_HauNgoc % 1 == 0 ? x2_53_57_HauNgoc.ToString("G") : x2_53_57_HauNgoc.ToString("0.##"))}";
+                // Trả về kết quả 2 nghiệm x1 và x2
+                return $"x1 = {FormatNumber_53_57_HauNgoc(x1_53_57_HauNgoc)}, " +
+                     $"x2 = {FormatNumber_53_57_HauNgoc(x2_53_57_HauNgoc)}";
             }
         }
     }
--- NUnit_53_57_HauNgoc/UnitTest_53_57_HauNgoc/UnitTest1.cs
+++ NUnit_53_57_HauNgoc/UnitTest_53_57_HauNgoc/UnitTest1.cs
@@ -10,143 +10,195 @@ namespace UnitTest_53_57_HauNgoc
     public class UnitTest1
     {
         // Tạo đối tượng TestContext
+        // TestContext cho phép test sử dụng dữ liệu từ bên ngoài như CSV, Excel
         public TestContext TestContext { get; set; }
 
+        //====================== CÁC TEST CASE PASS =======================
 
-        // Các test case PASS
         // Trở thành PT bậc nhất: a khác 0
-        //TC1: a = 0, b khác 0
+        // TC1: a = 0, b ≠ 0 => phương trình bậc nhất có nghiệm -> TC: Pass
         [TestMethod]
-        public void TC1_PTBacNhat_53_57_HauNgoc_Pass()
+        public void TC1_PTBacNhatCoNghiem_53_57_HauNgoc_Pass()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(0, 2, -4);
-            String actualResult = ptbac2.Solve_53_57_HauNgoc();
-            string expectedResult = "x = 2";
-            Assert.AreEqual(expectedResult, actualResult);
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 0, b = 2, c = -4
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(0, 2, -4);
+            // Gọi hàm giải đê có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
+            string expected_53_57_HauNgoc = "x = 2";
+            // So sánh 2 kết quả
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        //TC2: Bậc nhất vô nghiệm
+        // TC2: a = 0, b = 0, c ≠ 0 => Phương trình bậc nhất
+        // => Phương trình vô nghiệm -> TC: Pass
         [TestMethod]
         public void TC2_PTBacNhatVN_53_57_HauNgoc_Pass()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(0, 0, 5);
-            String actualResult = ptbac2.Solve_53_57_HauNgoc();
-            String expectedResult = "Phương trình vô nghiệm";
-            Assert.AreEqual(expectedResult, actualResult);
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 0, b = 0, c = 5
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(0, 0, 5);
+            // Gọi hàm giải đê có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
+            String expected_53_57_HauNgoc = "Phuong trinh vo nghiem";
+            // So sánh 2 kết quả
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        //TC3: Bậc nhất vô số nghiệm
+        // TC3: a = 0, b = 0, c = 0 => Phương trình bậc nhất
+        // => Phương trình có vô số nghiệm -> TC: Pass
         [TestMethod]
         public void TC3_PTBacNhatVSN_53_57_HauNgoc_Pass()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(0, 0, 0);
-            String actualResult = ptbac2.Solve_53_57_HauNgoc();
-            String expectedResult = "Phương trình có vô số nghiệm";
-            Assert.AreEqual(expectedResult, actualResult);
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 0, b = 0, c = 0
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(0, 0, 0);
+            // Gọi hàm giải đê có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
+            String expected_53_57_HauNgoc = "Phuong trinh co vo so nghiem";
+            // So sánh 2 kết quả
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-
         //Phương trình bậc 2: a khác 0
-        // TC4: Phương trình vô nghiệm, delta < 0
+        // TC4: Phương trình bậc 2 có delta < 0 -> Phương trình vô nghiệm -> TC: Pass
         [TestMethod]
         public void TC4_PTB2VoNghiem_53_57_HauNgoc_Pass()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(6, 1, 2);
-            String actual_53_57_HauNgoc = ptbac2.Solve_53_57_HauNgoc();
+            // // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 6, b = 1, c = 2
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(6, 1, 2);
+            // Gọi hàm giải để có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
             // Kết quả mong đợi
-            String expected_53_57_HauNgoc = "Phương trình vô nghiệm";
+            String expected_53_57_HauNgoc = "Phuong trinh vo nghiem";
+            // So sánh 2 kết quả
             Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        // TC5: Phương trình có nghiệm kép, delta = 0
+        // TC5: Phương trình bậc 2 có delta = 0 -> Phương trình có nghiệm kép -> TC: Pass
         [TestMethod]
         public void TC5_PTB2NghiemKep_53_57_HauNgoc_Pass()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, -2, 1);
-            String actual_53_57_HauNgoc = ptbac2.Solve_53_57_HauNgoc();
-            String expected_53_57_HauNgoc = "Nghiệm kép x = 1";
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 1, b = -2, c = 1
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(1, -2, 1);
+            // Gọi hàm giải đê có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
+            String expected_53_57_HauNgoc = "Nghiem kep x = 1";
+            // So sánh 2 kết quả
             Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        // TC6: Phương trình có 2 nghiệm phân biệt, delta > 0
+        // TC6: Phương trình bậc 2 có delta > 0 -> Phương trình có 2 nghiệm phân biệt -> TC: Pass 
         [TestMethod]
         public void TC6_PTB22NghiemPB_53_57_HauNgoc_Pass()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, -3, 2);
-            String actual_53_57_HauNgoc = ptbac2.Solve_53_57_HauNgoc();
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 1, b = -3, c = 2
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(1, -3, 2);
+            // Gọi hàm giải đê có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
             String expected_53_57_HauNgoc = "x1 = 2, x2 = 1";
+            // So sánh 2 kết quả
             Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        //TC7: Hệ số a, b, c có giá trị âm
+        //====================== CÁC TEST CASE FAIL =======================
+        // TC7: Phương trình có 2 nghiệm phân biệt -> TC: Fail
+        // Kết quả mong đợi là 2 nghiệm x1 và x2, nhưng kết quả thực tế khác với kq mong đợi -> TC: Fail
         [TestMethod]
-        public void TC7_Heso_Am_53_57_HauNgoc_Pass()
+        public void TC7_2NghiemPB_53_57_HauNgoc_Fail()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(-1, -2, -1);
-            String actualResult = ptbac2.Solve_53_57_HauNgoc();
-            String expectedResult = "Nghiệm kép x = -1";
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-        //=======================================================================
-        /// </summary>
-        // Các test case FAIL
-        // TC8: Phương trình có 2 nghiệm phân biệt -> Fail
-        [TestMethod]
-        public void TC8_2NghiemPB_53_57_HauNgoc_Fail()
-        {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(4, -2, 5);
-            String actual_53_57_HauNgoc = ptbac2.Solve_53_57_HauNgoc();
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 4, b = -2, c = 5
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(4, -2, 5);
+            // Gọi hàm giải đê có kết quả thực tế
+            String actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
             String expected_53_57_HauNgoc = "x1 = 3, x2 = 2.45";
+            // So sánh 2 kết quả
             Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        // TC9:Phương trình có ngiệm kép -> Fail
+        // TC8:Phương trình có ngiệm kép -> Fail
+        // Kết quả mong đợi là nghiệm kép, nhưng kết quả thực tế khác với kq mong đợi -> TC: Fail
         [TestMethod]
-        public void TC9_NghiemKep_53_57_HauNgoc_Fail()
+        public void TC8_NghiemKep_53_57_HauNgoc_Fail()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, -4, 4);
-            string actualResult = ptbac2.Solve_53_57_HauNgoc();
-            string expectedResult = "Nghiệm kép x = 3";
-            Assert.AreEqual(expectedResult, actualResult);
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 1, b = -4, c = 4
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(1, -4, 4);
+            // Gọi hàm giải đê có kết quả thực tế
+            string actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
+            string expected_53_57_HauNgoc = "Nghiem kep x = 3";
+            // So sánh 2 kết quả
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        // TC10: Phương trình vô nghiệm -> Fail
+        // TC9: Phương trình vô nghiệm -> Fail
+        // Kết quả mong đợi là phương trình vô nghiệm, nhưng kết quả thực tế khác với kq mong đợi -> TC: Fail
         [TestMethod]
-        public void TC10_PTVoNghiem_53_57_HauNgoc_Fail()
+        public void TC9_PTVoNghiem_53_57_HauNgoc_Fail()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, 2, 1);
-            string actualResult = ptbac2.Solve_53_57_HauNgoc();
-            string expectedResult = "Phương trình vô nghiệm";
-            Assert.AreEqual(expectedResult, actualResult);
+            // Tạo đối tượng phương trình bậc 2
+            // Truyền giá trị vào a = 1, b = 2, c = 1
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(1, 2, 1);
+            // Gọi hàm giải đê có kết quả thực tế
+            string actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            // Tạo biến chứa kết quả mong đợi
+            string expected_53_57_HauNgoc = "Phuong trinh vo nghiem";
+            // So sánh 2 kết quả
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        // TC11: Phương trình có vô số nghiệm -> Fail
+        // TC10: Phương trình bậc 1: Phương trình vô nghiệm -> Fail
+        // Kết quả mong đợi là phương trình vô nghiệm, nhưng kết quả thực tế khác với kq mong đợi -> TC: Fail
         [TestMethod]
-        public void TC11_PTVoSoNghiem_53_57_HauNgoc_Fail()
+        public void TC10_PTBacNhatVN_53_57_HauNgoc_Fail()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(0, 0, 0);
-            string actualResult = ptbac2.Solve_53_57_HauNgoc();
-            string expectedResult = "Phương trình vô nghiệm";
-            Assert.AreEqual(expectedResult, actualResult);
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(0, 5, 2);
+            string actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            string expected_53_57_HauNgoc = "Phuong trinh vo nghiem";
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
-        // TC12: Phương trình có 2 nghiệm phân biệt -> Fail
+        // TC11:Phương trình bậc 1: vô nghiệm .... -> Fail
+        // Kết quả mong đợi là phương trình vô nghiệm, nhưng kết quả thực tế khác với kq mong đợi
         [TestMethod]
-        public void TC12_2NghiemPB_53_57_HauNgoc_Fail()
+        public void TC11_PTBacNhatCoNghiem_53_57_HauNgoc_Fail()
         {
-            PTBac2_53_57_HauNgoc ptbac2 = new PTBac2_53_57_HauNgoc(1, -5, 6);
-            string actualResult = ptbac2.Solve_53_57_HauNgoc();
-            string expectedResult = "x1 = 1, x2 = 2";
-            Assert.AreEqual(expectedResult, actualResult);
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(0, 14, 5);
+            string actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            string expected_53_57_HauNgoc = "x = 10";
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
+        }
+
+        // TC12:Phương trình bậc 1: có vô số nghiệm -> Fail
+        // Kết quả mong đợi là có vô số nghiệm, nhưng kết quả thực tế khác với kq mong đợi
+        [TestMethod]
+        public void TC12_PTBacNhatVSN_53_57_HauNgoc_Fail()
+        {
+            PTBac2_53_57_HauNgoc ptbac2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(0, 7, 10);
+            string actual_53_57_HauNgoc = ptbac2_53_57_HauNgoc.Solve_53_57_HauNgoc();
+            string expected_53_57_HauNgoc = "Phuong trinh co vo so nghiem";
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
         //==================================================================
-        
+
         // Data drive: Đọc file CSV
         // Đọc file CSV
+        [DeploymentItem(@"Data_53_57_HauNgoc\DataTest_53_57_HauNgoc.csv")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
-            @".\Data_53_57_HauNgoc\DataTest_53_57_HauNgoc.csv", "DataTest_53_57_HauNgoc#csv",
-            DataAccessMethod.Sequential)]
+    @"|DataDirectory|\Data_53_57_HauNgoc\DataTest_53_57_HauNgoc.csv",
+    "DataTest_53_57_HauNgoc#csv", DataAccessMethod.Sequential)]
 
         [TestMethod]
         public void TC13_53_57_HauNgoc_TestWithDataSource()
@@ -155,39 +207,25 @@ namespace UnitTest_53_57_HauNgoc
             int a_53_57_HauNgoc = int.Parse(TestContext.DataRow[0].ToString());
             int b_53_57_HauNgoc = int.Parse(TestContext.DataRow[1].ToString());
             int c_53_57_HauNgoc = int.Parse(TestContext.DataRow[2].ToString());
+            string expected_53_57_HauNgoc = TestContext.DataRow[3].ToString();
+            // Kiểm tra cột thứ 4 còn kí tự nào không? (Trường hợp có 2 nghiệm x1 = ..., x2 = ...)
+            // Nếu có thì cộng chuỗi để đạt được kết quả mong đợi đầy đủ là x1 = ..., x2 = ...
+            if (!string.IsNullOrEmpty(TestContext.DataRow[4].ToString()))
+            {
+                expected_53_57_HauNgoc = expected_53_57_HauNgoc + ", " + TestContext.DataRow[4].ToString();
+            }
+
+            Console.WriteLine(expected_53_57_HauNgoc);
 
             // Giải phương trình với a, b, c
             PTBac2_53_57_HauNgoc ptb2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(a_53_57_HauNgoc, b_53_57_HauNgoc, c_53_57_HauNgoc);
+            // Giải pt để có kết quả đạt được
             string actual_53_57_HauNgoc = ptb2_53_57_HauNgoc.Solve_53_57_HauNgoc();
 
-            // Loại bỏ phần text dư thừa trong kết quả thực tế
-            actual_53_57_HauNgoc = actual_53_57_HauNgoc.Replace("x1 =", "").Replace("x2 =", "").Replace("x =", "").Trim();
 
-            // Đọc kết quả mong muốn từ CSV
-            string expected_53_Hau = string.Join(",", Enumerable.Range(3, TestContext.DataRow.ItemArray.Length - 3)
-                                                     .Select(i => TestContext.DataRow[i].ToString().Trim()))
-                                                     .Trim(',');
-
-            // Xử lý trường hợp expected bị rỗng
-            if (string.IsNullOrEmpty(expected_53_Hau))
-            {
-                expected_53_Hau = actual_53_57_HauNgoc;
-            }
-
-            Console.WriteLine($"Expected: {expected_53_Hau}, Actual: {actual_53_57_HauNgoc}");
-
-            // Kiểm tra nếu có nhiều nghiệm
-            if (expected_53_Hau.Contains(","))
-            {
-                var expectedSet = new HashSet<string>(expected_53_Hau.Split(',').Select(x => x.Trim()));
-                var actualSet = new HashSet<string>(actual_53_57_HauNgoc.Split(',').Select(x => x.Trim()));
-
-                CollectionAssert.AreEquivalent(expectedSet.ToList(), actualSet.ToList());
-            }
-            else
-            {
-                Assert.AreEqual(expected_53_Hau, actual_53_57_HauNgoc);
-            }
+            Console.WriteLine($"Expected: {expected_53_57_HauNgoc}, Actual: {actual_53_57_HauNgoc}");
+            // So sánh 2 kết quả với nhau
+            Assert.AreEqual(expected_53_57_HauNgoc, actual_53_57_HauNgoc);
         }
 
         // Đọc file excel
@@ -210,39 +248,12 @@ namespace UnitTest_53_57_HauNgoc
             PTBac2_53_57_HauNgoc ptb2_53_57_HauNgoc = new PTBac2_53_57_HauNgoc(a_53_57_HauNgoc, b_53_57_HauNgoc, c_53_57_HauNgoc);
             string actual_53_57_HauNgoc = ptb2_53_57_HauNgoc.Solve_53_57_HauNgoc();
 
-            // Loại bỏ phần text dư thừa trong kết quả thực tế
-            actual_53_57_HauNgoc = actual_53_57_HauNgoc.Replace("x1 =", "").Replace("x2 =", "").Replace("x =", "").Trim();
-
             // Đọc kết quả mong muốn từ Excel
             string expected_53_Hau = TestContext.DataRow[3].ToString();
 
-            // Xử lý trường hợp expected bị rỗng
-            if (string.IsNullOrEmpty(expected_53_Hau))
-            {
-                expected_53_Hau = actual_53_57_HauNgoc;
-            }
-
             Console.WriteLine($"Expected: {expected_53_Hau}, Actual: {actual_53_57_HauNgoc}");
-
-            // Kiểm tra nếu có nhiều nghiệm
-            if (expected_53_Hau.Contains(","))
-            {
-                var expectedSet = new HashSet<string>(expected_53_Hau.Split(',').Select(x => x.Trim()));
-                var actualSet = new HashSet<string>(actual_53_57_HauNgoc.Split(',').Select(x => x.Trim()));
-
-                CollectionAssert.AreEquivalent(expectedSet.ToList(), actualSet.ToList());
-            }
-            else
-            {
-                Assert.AreEqual(expected_53_Hau, actual_53_57_HauNgoc);
-            }
-        }
-        //=============================================================================
-        // Kiểm thử ngoại lệ
-        [TestMethod]
-        public void TC15_NhapKyTuKhongHopLe_53_57_HauNgoc()
-        {
-            Assert.ThrowsException<FormatException>(() => new PTBac2_53_57_HauNgoc(Convert.ToDouble("a"), 2, 3));
+            // So sánh 2 kết quả: kq mong đợi và kết quả đạt được
+            Assert.AreEqual(expected_53_Hau, actual_53_57_HauNgoc);
         }
     }
 }
